430541a0121c597df36bfecff354af4c0161915a
Bruno Deilhot <bruno.deyllot@gmail.com>
2022-05-29 18:35:58 +0100

    Changed verification middleware

--- src/security/index.ts
+++ src/security/index.ts
@@ -1,10 +1,6 @@
 import { RequestHandler } from "express";
 
 const urlVerification: RequestHandler = async (req, res, next) => {
-  if (req.headers.origin !== process.env.REQ_HOST) {
-    return res.status(403).json("Access denied");
-  }
-
   if (req.header("x-api-key") !== process.env.API_KEY)
     return res.status(403).json("Access denied - API");
 
